383dddfe6f0fb7967ef564c6e436b55249abfcea
my-U <myouth_u@naver.com>
2021-12-12 15:58:06 +0900

    정수N개의합

---
+++ 수학/_15596_정수N개의합.py
@@ -0,0 +1,9 @@
+def solve(a): # def: 함수 생성 시 사용하는 예약어
+    return sum(a) # 반환 시 return으로 반환
+
+# 참고 코드 - for문 이용
+# def solve(a):
+#     total = 0
+#     for i in a:
+#         total += i
+#     return total
