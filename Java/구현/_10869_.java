b92d80032898db2e78dc026ee325d4d0a59f13de
my-U <myouth_u@naver.com>
2022-11-01 16:11:47 +0900

    사칙연산

---
+++ Java/구현/_10869_.java
@@ -0,0 +1,20 @@
+package Java.구현;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _10869_ {
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(input.readLine());
+
+        int A = Integer.parseInt(str.nextToken());
+        int B = Integer.parseInt(str.nextToken());
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
