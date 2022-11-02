89d0806a39fa2c71e24e45ab6fec6acf75f0aa67
my-U <myouth_u@naver.com>
2022-11-02 21:51:33 +0900

    ??!

---
+++ Java/구현/_10926_.java
@@ -0,0 +1,11 @@
+package Java.구현;
+
+import java.io.*;
+
+public class _10926_ {
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(input.readLine() + "??!");
+    }
+}
