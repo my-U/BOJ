91a9806661f94ef6c0c4c15848da1dc5a0f77417
my-U <myouth_u@naver.com>
2022-11-07 23:27:30 +0900

    새싹

---
+++ Java/구현/_25083_.java
@@ -0,0 +1,12 @@
+package Java.구현;
+
+public class _25083_ {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
