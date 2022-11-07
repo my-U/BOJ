542ab2722f8019cf98cadb0bfb92275a944632ce
my-U <myouth_u@naver.com>
2022-11-07 23:24:16 +0900

    고양이와 개

---
+++ Java/구현/_10171_.java
@@ -0,0 +1,10 @@
+package Java.구현;
+
+public class _10171_ {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ Java/구현/_10172_.java
@@ -0,0 +1,11 @@
+package Java.구현;
+
+public class _10172_ {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
